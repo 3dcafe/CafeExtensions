239be4765101be71305a38b5c545768e98cdde8a
Nikolay Latin <gs3d@yandex.ru>
2023-09-25 12:02:45 +0300

    remove out libs for optimization review UriService remove ExceptionHandlingMiddleware not using for optimization

---
+++ Extensions/UrlExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System.Web;
+
+namespace CafeExtensions.Extensions;
+
+public static class UrlExtensions
+{
+    public static string AddQueryString(string url, Dictionary<string, string> parameters)
+    {
+        if (string.IsNullOrEmpty(url))
+            throw new ArgumentNullException(nameof(url));
+        if (parameters == null || parameters.Count == 0)
+            return url;
+        var uriBuilder = new UriBuilder(url);
+        var query = HttpUtility.ParseQueryString(uriBuilder.Query);
+        foreach (var parameter in parameters)
+        {
+            query[parameter.Key] = parameter.Value;
+        }
+        uriBuilder.Query = query.ToString();
+        return uriBuilder.Uri.AbsoluteUri;
+    }
+}
--- Filters/UriService.cs
+++ Filters/UriService.cs
@@ -1,32 +1,42 @@
-﻿using Microsoft.AspNetCore.WebUtilities;
+﻿using CafeExtensions.Extensions;
 
 namespace CafeExtensions.Filters
 {
     /// <summary>
-    /// Сервис генерации url для пагинации
+    /// Service for generating URLs for pagination.
     /// </summary>
     public class UriService : IUriService
     {
         private readonly string _baseUri;
         /// <summary>
-        /// Базовый констурктор
+        /// Initializes a new instance of the UriService class.
         /// </summary>
-        /// <param name="baseUri"></param>
+        /// <param name="baseUri">The base URI for the service.</param>
         public UriService(string baseUri)
         {
             _baseUri = baseUri;
         }
         /// <summary>
-        /// Метод получения url для пагинации
+        /// Gets the URI for pagination.
         /// </summary>
-        /// <param name="filter"></param>
-        /// <param name="route"></param>
-        /// <returns></returns>
+        /// <param name="filter">Pagination filter.</param>
+        /// <param name="route">Route for the URI.</param>
+        /// <returns>The URI for pagination.</returns>
         public Uri GetPageUri(PaginationFilter filter, string route)
         {
-            var _enpointUri = new Uri(string.Concat(_baseUri, route));
-            var modifiedUri = QueryHelpers.AddQueryString(_enpointUri.ToString(), "pageNumber", filter.PageNumber.ToString());
-            modifiedUri = QueryHelpers.AddQueryString(modifiedUri, "pageSize", filter.PageSize.ToString());
+            // Create the endpoint URI by concatenating the base URI and the route.
+            var endpointUri = new Uri(string.Concat(_baseUri, route));
+
+            // Modify the URI by adding query parameters for page number and page size.
+            var modifiedUri = UrlExtensions.AddQueryString(
+                endpointUri.ToString(),
+                new Dictionary<string, string>
+                {
+                    { "pageNumber", filter.PageNumber.ToString() },
+                    { "pageSize", filter.PageSize.ToString() }
+                });
+
+            // Return the modified URI.
             return new Uri(modifiedUri);
         }
     }
