b9522a3f3e988bdf621bab78b5372233ae51b81c
Nikolay Latin <gs3d@yandex.ru>
2023-09-26 20:28:36 +0300

    New method, `ToStringTime`, is designed to format a given date and time into a human-readable text format suitable for the Russian region. It takes two parameters:
    1. `date`: The long value representing the date and time that you want to format.
    2. `isLastLogin`: A boolean flag that indicates whether this formatting is for the last login timestamp.
    
    Here's what the method does:
    
    - It calculates the time span between the current date and time and the provided `date` parameter.
    - The method distinguishes between two scenarios: formatting for user-related timestamps (e.g., "Was online 5 minutes ago") and formatting for message-related timestamps (e.g., "12:30 PM" or "Monday").
    - For user-related timestamps (`isLastLogin` is `true`), it generates strings like "Was online just now," "Was online 5 minutes ago," "Was online 2 hours ago," "Was online 3 days ago," "Was online a week ago," "Was online 2 weeks ago," "Was online 3 weeks ago," or "Was online 4 weeks ago," depending on the time elapsed.
    - For message-related timestamps (`isLastLogin` is `false`), it produces timestamps in the format "HH:mm" for timestamps within the last 24 hours, short day names (e.g., "Mon," "Tue") for timestamps within the last 30 days, "dd.MM" format for timestamps within the last 12 months, and "dd.MM.yy" format for timestamps older than 12 months.
    
    This method is useful for displaying user and message timestamps in a user-friendly and context-appropriate manner, making it easier for users to understand when an action or message occurred.

--- DateTimeExtension.cs
+++ DateTimeExtension.cs
@@ -1,9 +1,147 @@
-﻿namespace CafeExtensions;
+﻿using System.Globalization;
+
+namespace CafeExtensions;
 /// <summary>
 /// Extension for work with DateTime of Type
 /// </summary>
 public static class DateTimeExtension
 {
+    /// <summary>
+    /// return date in text format ru region
+    /// </summary>
+    /// <param name="date">дата</param>
+    /// <param name="isLastLogin"></param>
+    /// <returns></returns>
+    public static string ToStringTime(long date, bool isLastLogin)
+    {
+        const int SECOND = 1;
+        const int MINUTE = 60 * SECOND;
+        const int HOUR = 60 * MINUTE;
+        const int DAY = 24 * HOUR;
+        const int MONTH = 30 * DAY;
+
+        var timeSpan = new TimeSpan(DateTime.Now.Ticks - date);
+        double delta = Math.Abs(timeSpan.TotalSeconds);
+        //Для пользователей
+        if (isLastLogin == true)
+        {
+            string template = "Был(а) ";
+            string templateBack = " назад";
+            if (delta < 60 * MINUTE)
+            {
+                int minute = Math.Abs(timeSpan.Minutes);
+                if (minute < 1)
+                    return template + "только что";
+                if (minute >= 11 && minute <= 19)
+                    return template + minute + " минут" + templateBack;
+                var i = minute % 10;
+                switch (i)
+                {
+                    case 1:
+                        return template + minute + " минуту" + templateBack;
+                    case 2:
+                    case 3:
+                    case 4:
+                        return template + minute + " минуты" + templateBack;
+                    default:
+                        return template + minute + " минут" + templateBack;
+                }
+            }
+            if (delta < 24 * HOUR)
+            {
+                if (timeSpan.Hours >= 11 && timeSpan.Hours <= 19)
+                    return template + timeSpan.Hours + " часов" + templateBack;
+                var i = timeSpan.Hours % 10;
+                switch (i)
+                {
+                    case 1:
+                        return template + timeSpan.Hours + " час" + templateBack;
+                    case 2:
+                    case 3:
+                    case 4:
+                        return template + timeSpan.Hours + " часа" + templateBack;
+                    default:
+                        return template + timeSpan.Hours + " часов" + templateBack;
+                }
+            }
+
+            if (delta < 30 * DAY)
+            {
+                if (timeSpan.Days < 7)
+                {
+                    var i = timeSpan.Days % 10;
+                    switch (i)
+                    {
+                        case 1:
+                            return template + timeSpan.Days + " день" + templateBack;
+                        case 5:
+                        case 6:
+                            return template + timeSpan.Days + " дней" + templateBack;
+                        default:
+                            return template + timeSpan.Days + " дня" + templateBack;
+                    }
+                }
+                if (timeSpan.Days >= 7 && timeSpan.Days < 14)
+                    return template + " неделю" + templateBack;
+                if (timeSpan.Days >= 14 && timeSpan.Days < 21)
+                    return template + "2 недели" + templateBack;
+                if (timeSpan.Days >= 21 && timeSpan.Days < 28)
+                    return template + "3 недели" + templateBack;
+                else
+                    return template + "4 недели" + templateBack;
+            }
+
+            if (delta < 12 * MONTH)
+            {
+                int months = Convert.ToInt32(Math.Floor((double)timeSpan.Days / 30));
+                if (months > 1 && months < 5)
+                    return template + months + " месяца" + templateBack;
+                if (months >= 5 && months < 12)
+                    return template + months + " месяцев" + templateBack;
+                else
+                    return template + "1 месяц" + templateBack;
+            }
+            else
+            {
+                int years = Convert.ToInt32(Math.Floor((double)timeSpan.Days / 365));
+                int number = years;
+                if (years > 20)
+                    number = number % 10;
+                switch (number)
+                {
+                    case 1:
+                        return template + years + " год" + templateBack;
+                    case 2:
+                    case 3:
+                    case 4:
+                        return template + years + " года" + templateBack;
+                    default:
+                        return template + years + " лет" + templateBack;
+                }
+            }
+        }
+        //Для сообщений
+        else
+        {
+            DateTime dateTime = new DateTime(date);
+            if (delta < 24 * HOUR)
+                return dateTime.ToString("HH:mm");
+            if (delta < 30 * DAY)
+                if (timeSpan.Days < 7)
+                {
+                    string dayWeek = CultureInfo.GetCultureInfo("ru-RU").DateTimeFormat.GetDayName(dateTime.DayOfWeek);
+                    char[] array = dayWeek.ToCharArray();
+                    string dayWeekShort = "";
+                    if (dayWeek == "вторник" || dayWeek == "среда")
+                        return String.Join(dayWeekShort, array[0], array[1]);
+                    return String.Join(dayWeekShort, array[0], array[2]);
+                }
+            if (delta < 12 * MONTH)
+                return dateTime.ToString("dd.MM");
+            else
+                return dateTime.ToString("dd.MM.yy");
+        }
+    }
     public static DateTime StartOfDay(this DateTime theDate)
     {
         return theDate.Date;
