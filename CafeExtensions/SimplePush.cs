620326f8fafb67d1017d701d771ecfa89ae7697d
Latin Nikolay <gs3d@yandex.ru>
2024-09-18 16:13:42 +0300

    update simplepush new model

--- CafeExtensions/SimplePush.cs
+++ CafeExtensions/SimplePush.cs
@@ -35,7 +35,33 @@ namespace CafeExtensions
         /// Color push on android
         /// </summary>
         public string? android_accent_color { get; set; } = null;
+        /// <summary>
+        /// Data on push message
+        /// </summary>
+        public DataSimplePush? data { get; set; } = null;
     }
+
+    public class DataSimplePush
+    {
+        public string? userId { get; set; }
+
+        public int? type { get; set; }
+
+#warning add this fields
+        //userId = from.Id,
+        //            inits = callerInits,
+        //            my_inits = callerInits,
+        //            ttl = 30000,
+        //            with_video = withVideo,
+        //            discard,
+        //            type = typePush,
+        //            avatar = from.Avatar ?? "",
+        //            my_avatar = from.Avatar ?? "",
+        //            phone = from.PhoneNumber ?? "-",
+        //            roomId,
+        //            callId
+    }
+
     /// <summary>
     /// Заголовок
     /// </summary>
