6fc212086d4458b8434d17a8b1b8ed71f913b735
Nikolay Latin <gs3d@yandex.ru>
2024-11-22 17:59:10 +0300

    return default http client

--- CafeExtensions/Repositories/HttpSimpleClientRepository.cs
+++ CafeExtensions/Repositories/HttpSimpleClientRepository.cs
@@ -1,6 +1,7 @@
 ﻿using System.Net;
+using System.Net.Http.Headers;
 using System.Net.Http.Json;
-using System.Text;
+using System.Text.Json;
 
 namespace CafeExtensions.Repositories;
 /// <summary>
@@ -10,179 +11,358 @@ public class HttpSimpleClientRepository : IDisposable
 {
     private string? Login { get; set; }
     private string? Password { get; set; }
-    private TimeSpan? Timeout { get; set; }
-    private Dictionary<string, string> _headers = new();
-
     /// <summary>
-    /// Default constructor.
+    /// Header custom
+    /// </summary>
+    private Dictionary<string, string> _headers = new();
+    /// <summary>
+    /// Default contructor
     /// </summary>
     public HttpSimpleClientRepository() { }
-
     /// <summary>
-    /// Constructor with basic authorization credentials.
+    /// Contructor with basic autrization login
     /// </summary>
-    /// <param name="Login">Login for basic authentication.</param>
-    /// <param name="Password">Password for basic authentication.</param>
+    /// <param name="Login"></param>
+    /// <param name="Password"></param>
     public HttpSimpleClientRepository(string Login, string Password)
     {
         this.Login = Login;
         this.Password = Password;
     }
-
     /// <summary>
-    /// Constructor with basic authorization and a custom timeout.
+    /// Get request
     /// </summary>
-    /// <param name="Login">Login for basic authentication.</param>
-    /// <param name="Password">Password for basic authentication.</param>
-    /// <param name="timeout">Custom timeout for HTTP requests.</param>
-    public HttpSimpleClientRepository(string Login, string Password, TimeSpan timeout)
-    {
-        this.Login = Login;
-        this.Password = Password;
-        Timeout = timeout;
-    }
-
-    /// <summary>
-    /// Constructor with a custom timeout only.
-    /// </summary>
-    /// <param name="timeout">Custom timeout for HTTP requests.</param>
-    public HttpSimpleClientRepository(TimeSpan timeout)
-    {
-        Timeout = timeout;
-    }
-
-    /// <summary>
-    /// Sends an HTTP GET request and returns the deserialized response.
-    /// </summary>
-    /// <typeparam name="T">Type of the expected response.</typeparam>
-    /// <param name="url">The URL to send the request to.</param>
-    /// <returns>A response wrapped in <see cref="RestClientResponse{T}"/>.</returns>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="url"></param>
+    /// <returns></returns>
     public async Task<RestClientResponse<T>> GetAsync<T>(string url)
     {
-        using var httpClient = CreateHttpClient();
+        using (var httpClient = new HttpClient())
+        {
+            if (_headers.Count > 0)
+            {
+                httpClient.DefaultRequestHeaders.Clear();
+                foreach (var item in _headers)
+                {
+                    httpClient.DefaultRequestHeaders.Add(item.Key, item.Value);
+                }
+            }
 
-        HttpResponseMessage response = await httpClient.GetAsync(url);
-        return await CreateRestClientResponse<T>(response);
+            if (Login?.Length > 0 && Password?.Length > 0)
+            {
+                var authenticationString = $"{Login}:{Password}";
+                var base64EncodedAuthenticationString = Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes(authenticationString));
+
+                // Remove additional Authorization header from above
+                if (httpClient.DefaultRequestHeaders.Contains("Authorization")) httpClient.DefaultRequestHeaders.Remove("Authorization");
+
+                httpClient.DefaultRequestHeaders.Add("Authorization", "Basic " + base64EncodedAuthenticationString);
+            }
+
+            var response = await httpClient.GetAsync(url);
+            var restClientResponse = new RestClientResponse<T>
+            {
+                StatusCode = (int)response.StatusCode,
+                StatusName = response.StatusCode,
+                Message = response.ReasonPhrase,
+                Response = response.StatusCode != HttpStatusCode.OK ?
+                    default :
+                    await response.Content.ReadFromJsonAsync<T>()
+            };
+            return restClientResponse;
+        }
     }
 
     /// <summary>
-    /// Sends an HTTP GET request without expecting a response body.
+    /// Get request without content
     /// </summary>
-    /// <param name="url">The URL to send the request to.</param>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="url"></param>
+    /// <returns></returns>
     public async Task GetUpdateAsync(string url)
     {
-        using var httpClient = CreateHttpClient();
-        await httpClient.GetAsync(url);
+        using (var httpClient = new HttpClient())
+        {
+            if (Login?.Length > 0 && Password?.Length > 0)
+            {
+                var authenticationString = $"{Login}:{Password}";
+                var base64EncodedAuthenticationString = Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes(authenticationString));
+                httpClient.DefaultRequestHeaders.Add("Authorization", "Basic " + base64EncodedAuthenticationString);
+            }
+            var response = await httpClient.GetAsync(url);
+        }
     }
 
     /// <summary>
-    /// Sends an HTTP POST request with an object as JSON content and deserializes the response.
+    /// Get file from URL
     /// </summary>
-    /// <typeparam name="T">Type of the expected response.</typeparam>
-    /// <param name="url">The URL to send the request to.</param>
-    /// <param name="objectToPost">The object to send as JSON content.</param>
-    /// <param name="formUrlencoded">Indicates if the content should be sent as form-urlencoded.</param>
-    /// <returns>A response wrapped in <see cref="RestClientResponse{T}"/>.</returns>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="url"></param>
+    /// <returns></returns>
+    public async Task<byte[]?> GetFile(string url)
+    {
+        using var httpClient = new HttpClient();
+        try
+        {
+            using var stream = await httpClient.GetStreamAsync(url);
+            byte[] buffer = new byte[16 * 1024];
+            using MemoryStream ms = new MemoryStream();
+            int read;
+            while ((read = stream.Read(buffer, 0, buffer.Length)) > 0)
+            {
+                ms.Write(buffer, 0, read);
+            }
+            return ms.ToArray();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Ошибка получения файла: - " + url + ex.Message);
+            return null;
+        }
+    }
+
+    /// <summary>
+    /// Post request
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="url"></param>
+    /// <param name="objectToPost"></param>
+    /// <returns></returns>
     public async Task<RestClientResponse<T>?> PostAsync<T>(string url, object objectToPost, bool formUrlencoded = false)
     {
-        using var httpClient = CreateHttpClient();
-
-        HttpResponseMessage response;
-        if (formUrlencoded)
+        Console.WriteLine($"post->{url}");
+        using (var httpClient = new HttpClient())
         {
-            var dict = (Dictionary<string, string>)objectToPost;
-            response = await httpClient.PostAsync(url, new FormUrlEncodedContent(dict));
-        }
-        else
-        {
-            response = await httpClient.PostAsJsonAsync(url, objectToPost);
-        }
+            if (_headers.Count > 0)
+            {
+                httpClient.DefaultRequestHeaders.Clear();
+                foreach (var item in _headers)
+                {
+                    httpClient.DefaultRequestHeaders.Add(item.Key, item.Value);
+                }
+            }
 
-        return await CreateRestClientResponse<T>(response);
+            if (Login?.Length > 0 && Password?.Length > 0)
+            {
+                var authenticationString = $"{Login}:{Password}";
+                var base64EncodedAuthenticationString = Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes(authenticationString));
+                httpClient.DefaultRequestHeaders.Add("Authorization", "Basic " + base64EncodedAuthenticationString);
+            }
+
+            if (formUrlencoded)
+            {
+                var dict = (Dictionary<string, string>)objectToPost;
+                var req = new HttpRequestMessage(HttpMethod.Post, url) { Content = new FormUrlEncodedContent(dict) };
+                var response = await httpClient.SendAsync(req);
+                try
+                {
+                    var restClientResponse = new RestClientResponse<T>
+                    {
+                        StatusCode = (int)response.StatusCode,
+                        StatusName = response.StatusCode,
+                        Message = response.ReasonPhrase,
+                        Response = response.StatusCode != HttpStatusCode.OK ?
+                            default :
+                            await response.Content.ReadFromJsonAsync<T>()
+                    };
+                    return restClientResponse;
+                }
+                catch (Exception)
+                {
+                    return default;
+                }
+            }
+            else
+            {
+                var response = await httpClient.PostAsJsonAsync(url, objectToPost);
+#if DEBUG
+                string jsonTest = JsonSerializer.Serialize(objectToPost);
+                string jsonResponse = await response.Content.ReadAsStringAsync();
+#endif
+                try
+                {
+                    var restClientResponse = new RestClientResponse<T>
+                    {
+                        StatusCode = (int)response.StatusCode,
+                        StatusName = response.StatusCode,
+                        Message = response.ReasonPhrase,
+                        Response = response.StatusCode != HttpStatusCode.OK ?
+                            default :
+                            await response.Content.ReadFromJsonAsync<T>()
+                    };
+                    return restClientResponse;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                    return default;
+                }
+            }
+        }
+    }
+
+
+    /// <summary>
+    /// Post request
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="url"></param>
+    /// <param name="objectToPost"></param>
+    /// <returns></returns>
+    public async Task<bool> PostAsyncWithoutAnswer(string url, object objectToPost)
+    {
+        using (var httpClient = new HttpClient())
+        {
+            if (_headers.Count > 0)
+            {
+                httpClient.DefaultRequestHeaders.Clear();
+                foreach (var item in _headers)
+                {
+                    httpClient.DefaultRequestHeaders.Add(item.Key, item.Value);
+                }
+            }
+            if (Login?.Length > 0 && Password?.Length > 0)
+            {
+                var authenticationString = $"{Login}:{Password}";
+                var base64EncodedAuthenticationString = Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes(authenticationString));
+                httpClient.DefaultRequestHeaders.Add("Authorization", "Basic " + base64EncodedAuthenticationString);
+            }
+
+            var response = await httpClient.PostAsJsonAsync(url, objectToPost);
+            try
+            {
+                return response.StatusCode != HttpStatusCode.OK ? false : true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                return false;
+            }
+        }
     }
 
     /// <summary>
-    /// Sets custom headers for HTTP requests.
+    /// Post request for get stream file
     /// </summary>
-    /// <param name="dictionary">A dictionary of headers to set.</param>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="url"></param>
+    /// <param name="objectToPost"></param>
+    /// <returns></returns>
+    public async Task<Stream?> PostGetStreamAsync(string url, object objectToPost)
+    {
+        using (var httpClient = new HttpClient())
+        {
+            if (_headers.Count > 0)
+            {
+                httpClient.DefaultRequestHeaders.Clear();
+                foreach (var item in _headers)
+                {
+                    httpClient.DefaultRequestHeaders.Add(item.Key, item.Value);
+                }
+            }
+
+            if (Login?.Length > 0 && Password?.Length > 0)
+            {
+                var authenticationString = $"{Login}:{Password}";
+                var base64EncodedAuthenticationString = Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes(authenticationString));
+                httpClient.DefaultRequestHeaders.Add("Authorization", "Basic " + base64EncodedAuthenticationString);
+            }
+
+            var response = await httpClient.PostAsJsonAsync(url, objectToPost);
+            try
+            {
+                return await response.Content.ReadAsStreamAsync();
+            }
+            catch (Exception ex)
+            {
+                Console.Write(ex.ToString());
+                return null;
+            }
+        }
+    }
+
+
+
+    public async Task<RestClientResponse<T>> FileUplopadAsync<T>(string url, string FileName, byte[] fileData)
+    {
+        using (var httpClient = new HttpClient())
+        {
+            if (_headers.Count > 0)
+            {
+                httpClient.DefaultRequestHeaders.Clear();
+                foreach (var item in _headers)
+                {
+                    httpClient.DefaultRequestHeaders.Add(item.Key, item.Value);
+                }
+            }
+
+            if (Login?.Length > 0 && Password?.Length > 0)
+            {
+                var authenticationString = $"{Login}:{Password}";
+                var base64EncodedAuthenticationString = Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes(authenticationString));
+                httpClient.DefaultRequestHeaders.Add("Authorization", "Basic " + base64EncodedAuthenticationString);
+            }
+
+            using (var multipartFormContent = new MultipartFormDataContent())
+            {
+                var byteContent = new ByteArrayContent(fileData);
+                byteContent.Headers.ContentType = new MediaTypeHeaderValue("image/png");
+                multipartFormContent.Add(byteContent, name: "uploadedFiles", fileName: FileName);
+
+                var response = await httpClient.PostAsync(url, multipartFormContent);
+                try
+                {
+                    var restClientResponse = new RestClientResponse<T>
+                    {
+                        StatusCode = (int)response.StatusCode,
+                        StatusName = response.StatusCode,
+                        Message = response.ReasonPhrase,
+                        Response = response.StatusCode != HttpStatusCode.OK ?
+                        default :
+                        await response.Content.ReadFromJsonAsync<T>()
+                    };
+                    return restClientResponse;
+                }
+                catch (Exception ex)
+                {
+                    Console.Write(ex.ToString());
+                    var restClientResponse = new RestClientResponse<T>
+                    {
+                        StatusCode = (int)response.StatusCode,
+                        StatusName = response.StatusCode,
+                        Message = response.ReasonPhrase,
+                        Response = default
+                    };
+                    return restClientResponse;
+                }
+            }
+        }
+    }
+
+
     public void SetHeaders(Dictionary<string, string> dictionary)
     {
-        _headers.Clear();
+        this._headers.Clear();
         foreach (var item in dictionary)
         {
             _headers.Add(item.Key, item.Value);
         }
     }
-
     /// <summary>
-    /// Sets the OrganizationId header for HTTP requests.
+    /// Set headers with OrganizationId key
     /// </summary>
-    /// <param name="organizationId">The OrganizationId value to set.</param>
+    /// <param name="organizationId"></param>
     public void SetHeaders(string organizationId)
     {
-        _headers = new Dictionary<string, string>
+        this._headers =
+            new Dictionary<string, string>()
             {
-                { "OrganizationId", organizationId }
+                    { "OrganizationId" , organizationId }
             };
     }
 
-    /// <summary>
-    /// Disposes resources used by the class.
-    /// </summary>
     public void Dispose()
     {
         GC.SuppressFinalize(this);
-    }
-
-    /// <summary>
-    /// Creates and configures an <see cref="HttpClient"/> instance.
-    /// </summary>
-    /// <returns>A configured <see cref="HttpClient"/>.</returns>
-    private HttpClient CreateHttpClient()
-    {
-        var httpClient = new HttpClient();
-        if (Timeout.HasValue)
-        {
-            httpClient.Timeout = Timeout.Value;
-        }
-
-        if (_headers.Count > 0)
-        {
-            httpClient.DefaultRequestHeaders.Clear();
-            foreach (var header in _headers)
-            {
-                httpClient.DefaultRequestHeaders.Add(header.Key, header.Value);
-            }
-        }
-
-        if (!string.IsNullOrEmpty(Login) && !string.IsNullOrEmpty(Password))
-        {
-            var authenticationString = $"{Login}:{Password}";
-            var base64EncodedAuthenticationString = Convert.ToBase64String(Encoding.UTF8.GetBytes(authenticationString));
-            httpClient.DefaultRequestHeaders.Add("Authorization", "Basic " + base64EncodedAuthenticationString);
-        }
-
-        return httpClient;
-    }
-
-    /// <summary>
-    /// Creates a <see cref="RestClientResponse{T}"/> from an <see cref="HttpResponseMessage"/>.
-    /// </summary>
-    /// <typeparam name="T">Type of the expected response.</typeparam>
-    /// <param name="response">The HTTP response message.</param>
-    /// <returns>A <see cref="RestClientResponse{T}"/> instance.</returns>
-    private async Task<RestClientResponse<T>> CreateRestClientResponse<T>(HttpResponseMessage response)
-    {
-        var restClientResponse = new RestClientResponse<T>
-        {
-            StatusCode = (int)response.StatusCode,
-            StatusName = response.StatusCode,
-            Message = response.ReasonPhrase,
-            Response = response.StatusCode == HttpStatusCode.OK
-                ? await response.Content.ReadFromJsonAsync<T>()
-                : default
-        };
-
-        return restClientResponse;
     }
 }
